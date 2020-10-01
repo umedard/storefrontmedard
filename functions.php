e49bb9b09bd1c5484ca22fad612bba6e0f6de071
Medard <umedard@gmail.com>
2020-10-01 21:02:15 +0200

    Add scripts to header

--- functions.php
+++ functions.php
@@ -1,4 +1,13 @@
 <?php
 
-// wp_enqueue_style( 'style', get_stylesheet_uri() );
+function scripts_setup() {
+wp_enqueue_style( 'main-style', get_stylesheet_uri() ); //default
+
+wp_enqueue_style( 'custom-style', get_template_directory_uri() . '/dist/css/styles.css' ); //our stylesheet
+wp_enqueue_script('main-script',  get_template_directory_uri() . '/dist/js/bundle.js');
+
+};
+
+add_action( 'wp_enqueue_scripts', 'script_setup' );
+
 ?>
--- header.php
+++ header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php the_title('<title>', '</title>') ?>
+    <meta name="description" content="<?php the_excerpt(); ?>" />
+    <?php wp_head() ?>
 </head>
-<body>
-    
-</body>
-</html>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
+<div class="test">t</div>
