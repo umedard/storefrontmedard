7c5422b74f6306d27ae8c8c1a296489f1d434ec3
Medard <umedard@gmail.com>
2020-10-04 22:55:11 +0200

    title fix

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php the_title('<title>', '</title>') ?>
     <meta name="description" content="<?php the_excerpt(); ?>" />
     <?php wp_head() ?>
 </head>
