e03e788afd5353caae23116e59cbd007e5318910
Medard <umedard@gmail.com>
2020-10-04 23:08:20 +0200

    Add woocommerce support

--- functions.php
+++ functions.php
@@ -162,7 +162,24 @@ function load_dashicons() {
  wp_enqueue_style( 'dashicons' );
 }
 
-// custom read more 
+// configure WooCommerce
+
+function add_woocommerce_support() {
+	add_theme_support( 'woocommerce', array(
+		'thumbnail_image_width' => 150,
+		'single_image_width'    => 300,
+
+        'product_grid'          => array(
+            'default_rows'    => 3,
+            'min_rows'        => 2,
+            'max_rows'        => 8,
+            'default_columns' => 4,
+            'min_columns'     => 2,
+            'max_columns'     => 5,
+        ),
+	) );
+}
+add_action( 'after_setup_theme', 'add_woocommerce_support' );
 
 
 
