4832ef29936a76233685f19a2d2bd511b30d8728
Medard <umedard@gmail.com>
2020-10-05 09:42:34 +0200

    remove shop

--- functions.php
+++ functions.php
@@ -164,24 +164,27 @@ function load_dashicons() {
 
 // configure WooCommerce
 
-function add_woocommerce_support() {
-	add_theme_support( 'woocommerce', array(
-		'thumbnail_image_width' => 150,
-		'single_image_width'    => 300,
+// function add_woocommerce_support() {
+// 	add_theme_support( 'woocommerce', array(
+// 		'thumbnail_image_width' => 150,
+// 		'single_image_width'    => 300,
 
-        'product_grid'          => array(
-            'default_rows'    => 3,
-            'min_rows'        => 2,
-            'max_rows'        => 8,
-            'default_columns' => 4,
-            'min_columns'     => 2,
-            'max_columns'     => 5,
-        ),
-	) );
-}
-add_action( 'after_setup_theme', 'add_woocommerce_support' );
+//         'product_grid'          => array(
+//             'default_rows'    => 3,
+//             'min_rows'        => 2,
+//             'max_rows'        => 8,
+//             'default_columns' => 4,
+//             'min_columns'     => 2,
+//             'max_columns'     => 5,
+//         ),
+// 	) );
+// }
+// add_action( 'after_setup_theme', 'add_woocommerce_support' );
 
-
+// function mytheme_add_woocommerce_support() {
+// 	add_theme_support( 'woocommerce' );
+// }
+// add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
 
 ?>
