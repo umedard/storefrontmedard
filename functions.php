bb43a8701f016e64955510534875f3db833fa1aa
Medard <umedard@gmail.com>
2020-10-05 13:06:08 +0200

    Add woocommerce theme files

--- functions.php
+++ functions.php
@@ -186,5 +186,9 @@ function load_dashicons() {
 // }
 // add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
+function mytheme_add_woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
 ?>
