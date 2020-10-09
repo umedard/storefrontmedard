ac49a655ab140eb6544900180a23ee9d2989e3c3
Medard <umedard@gmail.com>
2020-10-10 00:25:55 +0200

    improve menu (js), RWD for small screens

--- src/js/mobilemenu.js
+++ src/js/mobilemenu.js
@@ -1,1 +1,10 @@
-console.log("test");
+const menu = () => {
+  const buttons = document.querySelectorAll(".menu-item-has-children > a");
+  for (const button of buttons) {
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+    });
+  }
+};
+
+window.addEventListener("load", menu);
